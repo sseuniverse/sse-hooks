2ddee3faaf04c8fb88e020bfce404dab4ae24224
SSE World <114757626+sseworld@users.noreply.github.com>
2026-02-10 07:25:24 +0530

    feat: add API endpoints for fetching hook and schema metadata

---
+++ app/www/server/api/registry/hook/manifest.json.get.ts
@@ -0,0 +1,21 @@
+import { ofetch } from "ofetch";
+import { defineEventHandler, createError } from "h3";
+
+const BASE_URL =
+  "https://raw.githubusercontent.com/sseuniverse/sse-hooks/refs/heads/main/packages/hooks/manifest.json";
+
+export default defineEventHandler(async (event) => {
+  try {
+    const metaData = await ofetch(BASE_URL, {
+      parseResponse: JSON.parse,
+    });
+
+    return metaData;
+  } catch (error: any) {
+    // Handle 404s if the hook or the meta.json doesn't exist
+    throw createError({
+      statusCode: error.response?.status || 404,
+      statusMessage: `Meta schema file for not found at ${BASE_URL}`,
+    });
+  }
+});
